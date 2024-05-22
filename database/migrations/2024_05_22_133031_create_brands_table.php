f86988d4bfbf3eb1b8989f9fa2dbc1c807061d40
najmusskb <najmussakib173@gmail.com>
2024-05-22 20:01:26 +0600

    crud of brand

---
+++ database/migrations/2024_05_22_133031_create_brands_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('is_featured')->default(0)->comment('0 is normal, 1 for Featured');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 Route::group(['prefix'=> 'admin'],function(){
-    route::get('/dashboard','App\Http\Controllers\Backend\pagesController@index');
+    route::get('/dashboard','App\Http\Controllers\Backend\pagesController@index')->name('dashboard');
 
-    route::get('/text','App\Http\Controllers\Backend\pagesController@text');
+    // Brand Route for Route
+    Route::group(['prefix'=>'brand'],function(){
+        Route::get('/manage','App\Http\Controllers\Backend\BrandController@index')->name('brand.manage');
+
+        Route::get('/create','App\Http\Controllers\Backend\BrandController@create')->name('brand.create');
+
+        Route::post('/store','App\Http\Controllers\Backend\BrandController@store')->name('brand.store');
+
+        Route::get('/edit/{id}','App\Http\Controllers\Backend\BrandController@edit')->name('brand.edit');
+
+        Route::post('/edit/{id}','App\Http\Controllers\Backend\BrandController@update')->name('brand.edit');
+
+        Route::post('/delete/{id}','App\Http\Controllers\Backend\BrandController@destroy')->name('brand.destroy');
+
+    });
 });
